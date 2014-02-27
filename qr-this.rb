f7f2294942b04677965cc7cc7a47c91ce256a716
Daniel Smith <jellymann@gmail.com>
2014-02-27 03:24:39 +0200

    Should work off any server now

--- qr-this.rb
+++ qr-this.rb
@@ -3,6 +3,12 @@ require 'slim'
 
 get('/') { slim :index }
 get('/qrthis') do
+
+
+  @host = request.host
+  port = request.port
+  @host = "#{@host}:#{port}" unless port == 80
+
   @url = params[:url]
   slim :qrthis
 end
